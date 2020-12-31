5d2b9577b5c507ff4cd2b27f32233d12fa4da969
jainshi <jainshi@gmail.com>
2020-12-31 21:14:06 +0100

    media-manager
    - some refactoring to move directory type and tag information close to mediatype enum constants

--- src/main/java/com/jainshi/app/model/MediaType.java
+++ src/main/java/com/jainshi/app/model/MediaType.java
@@ -1,20 +1,38 @@
 package com.jainshi.app.model;
 
+import com.drew.metadata.Directory;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.mov.QuickTimeDirectory;
+import com.drew.metadata.mov.metadata.QuickTimeMetadataDirectory;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.Optional;
 
+import static com.drew.metadata.exif.ExifDirectoryBase.*;
+import static com.drew.metadata.mov.QuickTimeDirectory.TAG_CREATION_TIME;
+import static com.drew.metadata.mov.QuickTimeDirectory.TAG_MODIFICATION_TIME;
+
 @Getter
 @AllArgsConstructor
 public enum MediaType {
 
-    IMAGE("image"),
-    VIDEO("video"),
-    UNKNOWN("unknown");
+    IMAGE("image",
+            ExifSubIFDDirectory.class, TAG_DATETIME_ORIGINAL, TAG_DATETIME_DIGITIZED,
+            ExifIFD0Directory.class, TAG_MODEL, TAG_MAKE),
+    VIDEO("video",
+            QuickTimeDirectory.class, TAG_CREATION_TIME, TAG_MODIFICATION_TIME,
+            QuickTimeMetadataDirectory.class, QuickTimeMetadataDirectory.TAG_MODEL, QuickTimeMetadataDirectory.TAG_MAKE);
 
     private String mimePrefix;
+    private Class<? extends Directory> createModifiedTimeDirectory;
+    private int createTimeTag;
+    private int modifiedTimeTag;
+    private Class<? extends Directory> makeModelDirectory;
+    private int modelTag;
+    private int makeTag;
 
     public static MediaType byMimeType(String mimeType) {
         return Optional.ofNullable(mimeType)
@@ -22,6 +40,6 @@ public enum MediaType {
                         mime -> Arrays.stream(MediaType.values())
                                 .filter(mediaType -> mime.startsWith(mediaType.getMimePrefix()))
                                 .findFirst())
-                .orElse(UNKNOWN);
+                .orElse(null);
     }
 }
--- src/main/java/com/jainshi/app/util/image/ImageInfoReader.java
+++ src/main/java/com/jainshi/app/util/image/ImageInfoReader.java
@@ -4,11 +4,7 @@ import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
-import com.drew.metadata.exif.ExifIFD0Directory;
-import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.drew.metadata.file.FileTypeDirectory;
-import com.drew.metadata.mov.QuickTimeDirectory;
-import com.drew.metadata.mov.metadata.QuickTimeMetadataDirectory;
 import com.jainshi.app.model.ImageInfo;
 import com.jainshi.app.model.MediaType;
 import lombok.SneakyThrows;
@@ -22,7 +18,6 @@ import java.time.ZoneId;
 import java.util.Date;
 import java.util.Optional;
 
-import static com.drew.metadata.exif.ExifDirectoryBase.*;
 import static com.drew.metadata.file.FileTypeDirectory.TAG_DETECTED_FILE_MIME_TYPE;
 import static com.drew.metadata.file.FileTypeDirectory.TAG_EXPECTED_FILE_NAME_EXTENSION;
 
@@ -44,29 +39,28 @@ public class ImageInfoReader {
                     String mimeType = extractString(meta, FileTypeDirectory.class, TAG_DETECTED_FILE_MIME_TYPE);
                     imageInfo.setExtension(extractString(meta, FileTypeDirectory.class, TAG_EXPECTED_FILE_NAME_EXTENSION));
 
-                    switch (MediaType.byMimeType(mimeType)) {
-                        case IMAGE :
-                            imageInfo.setCreatedOn(asLocalDateTime(extractDate(meta, ExifSubIFDDirectory.class, TAG_DATETIME_ORIGINAL)));
-                            imageInfo.setModifiedOn(asLocalDateTime(extractDate(meta, ExifSubIFDDirectory.class, TAG_DATETIME_DIGITIZED)));
-                            imageInfo.setModel(extractString(meta, ExifIFD0Directory.class, TAG_MODEL));
-                            imageInfo.setMake(extractString(meta, ExifIFD0Directory.class, TAG_MAKE));
-                            break;
-                        case VIDEO:
-                            imageInfo.setCreatedOn(asLocalDateTime(extractDate(meta, QuickTimeDirectory.class, QuickTimeDirectory.TAG_CREATION_TIME)));
-                            imageInfo.setModifiedOn(asLocalDateTime(extractDate(meta, QuickTimeDirectory.class, QuickTimeDirectory.TAG_MODIFICATION_TIME)));
-                            imageInfo.setModel(extractString(meta, QuickTimeMetadataDirectory.class, QuickTimeMetadataDirectory.TAG_MODEL));
-                            imageInfo.setMake(extractString(meta, QuickTimeMetadataDirectory.class, QuickTimeMetadataDirectory.TAG_MAKE));
-                            break;
-                        default:
-                            log.error("Unknown mime type: {}", mimeType);
-                    }
+                    Optional.ofNullable(MediaType.byMimeType(mimeType))
+                            .ifPresentOrElse(mediaType -> {
+                                imageInfo.setCreatedOn(asLocalDateTime(extractDate(meta,
+                                        mediaType.getCreateModifiedTimeDirectory(),
+                                        mediaType.getCreateTimeTag())));
+                                imageInfo.setModifiedOn(asLocalDateTime(extractDate(meta,
+                                        mediaType.getCreateModifiedTimeDirectory(),
+                                        mediaType.getModifiedTimeTag())));
+                                imageInfo.setMake(extractString(meta,
+                                        mediaType.getMakeModelDirectory(),
+                                        mediaType.getMakeTag()));
+                                imageInfo.setModel(extractString(meta,
+                                        mediaType.getMakeModelDirectory(),
+                                        mediaType.getModelTag()));
+                            }, () ->  log.warn("Unknown mime type: {}", mimeType) );
 
                 });
 
         return imageInfo;
     }
 
-    private static Optional<Metadata> getMetadata(File imagePath) throws ImageProcessingException, IOException {
+    private static Optional<Metadata> getMetadata(File imagePath) {
         try {
             return Optional.ofNullable(ImageMetadataReader.readMetadata(imagePath));
         } catch (ImageProcessingException | IOException e) {
